b51c5cdc15617bdbc07ac67f2311eefb5dcb0492
Your Name <you@example.com>
2024-12-19 18:05:35 +0000

    si

---
+++ 01-setup-gke.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source .env
+
+gcloud container clusters create gke-loki --zone us-central1-a --project ${PROJECT}
+
+gcloud container clusters get-credentials gke-loki --zone us-central1-a --project ${PROJECT}
+
+kubectl get nodes
---
+++ 04-deploy-loki.bash
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+source .env
+
+NS=loki
+KEY_FILE=sm.json
+SA=${SA_NAME}@${PROJECT}.iam.gserviceaccount.com
+SECRET=gcp-sa
+
+kubectl create ns ${NS}
+
+# Create service account secret
+gcloud iam service-accounts keys create ${KEY_FILE} --iam-account=${SA}
+kubectl delete secret ${SECRET} -n ${NS}
+kubectl create secret generic ${SECRET} --from-file=gcp-sa-file=${KEY_FILE} -n ${NS}
+
+
+helm repo add loki-helm https://grafana.github.io/helm-charts/
+
+helm template loki loki-helm/loki-distributed \
+-f loki/loki.yaml \
+-f loki/loki-volume.yaml \
+--set customParams.gcsBucket=${BUCKET_NAME} \
+--version 0.45.1 \
+--namespace ${NS} > tmp-loki.yaml
+
+kubectl apply -n ${NS} -f tmp-loki.yaml
+
+kubectl apply -n ${NS} -f loki/loki-ing.yaml
